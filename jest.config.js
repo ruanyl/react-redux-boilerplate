6b21b729d02a725f45581a2fd712711d8927a350
Yulong Ruan <yruan@alpha-sense.com>
2018-07-08 15:54:26 +0300

    fixed jest does not support non js/ts asssets

--- jest.config.js
+++ jest.config.js
@@ -18,4 +18,9 @@ module.exports = {
     '!src/**/*.d.ts',
   ],
   setupTestFrameworkScriptFile: './setupTests.ts',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.ts',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.ts'
+  }
 };
